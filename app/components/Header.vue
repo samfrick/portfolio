80749ba1b4159e90cc224d7de857f4bed484a165
Salomé Frick <salome.frick@free.fr>
2026-03-02 20:48:12 +0100

    update header

--- app/components/Header.vue
+++ app/components/Header.vue
@@ -10,16 +10,10 @@ const items = <NavigationMenuItem[]>[
 </script>
 
 <template>
-    <div class="fixed top-2 sm:top-4 mx-auto left-1/2 transform -translate-x-1/2 z-10">
-        <UNavigationMenu :items="items" variant="link" color="neutral"
-            class="bg-muted/80 backdrop-blur-sm rounded-full px-2 sm:px-4 border border-muted/50 shadow-lg shadow-neutral-950/5"
-            :ui="{
-                link: 'px-2 py-1',
-                linkLeadingIcon: 'hidden'
-            }">
-            <template #list-trailing>
-                <UColorModeButton />
-            </template>
-        </UNavigationMenu>
-    </div>
+    <UHeader title="" class="sticky">
+        <UNavigationMenu :items="items" color="neutral" />
+        <template #right>
+            <UColorModeButton />
+        </template>
+    </UHeader>
 </template>
