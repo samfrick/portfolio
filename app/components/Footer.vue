a4ba71022c2c19d31e6bdbfa8c8222e8aa70d6ea
Salomé Frick <salome.frick@free.fr>
2026-03-02 20:12:02 +0100

    create footer

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,1 +1,38 @@
-<template></template>
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items: NavigationMenuItem[] = [
+    {
+        label: 'Figma Kit',
+        to: 'https://go.nuxt.com/figma-ui',
+        target: '_blank'
+    },
+    {
+        label: 'Playground',
+        to: 'https://stackblitz.com/edit/nuxt-ui',
+        target: '_blank'
+    },
+    {
+        label: 'Releases',
+        to: 'https://github.com/nuxt/ui/releases',
+        target: '_blank'
+    }
+]
+</script>
+
+<template>
+    <UFooter>
+        <template #left>
+            <p class="text-muted text-sm">Copyright © {{ new Date().getFullYear() }}</p>
+        </template>
+
+        <UNavigationMenu :items="items" variant="link" />
+
+        <template #right>
+            <UButton icon="i-simple-icons-github" color="neutral" variant="ghost" to="https://github.com/samfrick"
+                target="_blank" aria-label="GitHub" />
+            <UButton icon="i-simple-icons-linkedin" color="neutral" variant="ghost"
+                to="https://www.linkedin.com/in/salomé-frick-636034294" target="_blank" aria-label="LinkedIn" />
+        </template>
+    </UFooter>
+</template>
