0f8f70efa03156403cdfb77a5fc61169d852279d
Salomé Frick <salome.frick@free.fr>
2026-03-09 23:23:57 +0100

    create page projects test

---
+++ app/pages/projects.vue
@@ -0,0 +1,11 @@
+<script setuo lang="ts"></script>
+
+<template>
+    <section class="w-full h-full flex flex-col justify-center items-center">
+        <UPageHero title="Mes projets" description="Découvrez ici certains de mes projets" orientation="horizontal"
+            reverse>
+            <img src="https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg"
+                alt="fraise" class="rounded-full w-80 aspect-square object-cover">
+        </UPageHero>
+    </section>
+</template>
