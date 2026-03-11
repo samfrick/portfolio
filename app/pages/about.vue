4df4bd0e7a0fec70e4b312df33f7b0e0c84b6fd4
Salomé Frick <salome.frick@free.fr>
2026-03-11 19:52:14 +0100

    update page about

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,40 +1,56 @@
 <script setup lang="ts">
 import type { TimelineItem } from '@nuxt/ui'
 
-const items = ref<TimelineItem[]>([
+const aboutMe = 'Motivée, dynamique, curieuse et attentive, je suis actuellement étudiante en 3ème année de BUT Métiers du Multimédia et de l\'Internet, à l\'IUT de Haguenau (Bas-Rhin). \
+                Je suis actuellement alternante développeuse web front-end chez Logitud Solutions. \
+                J\'ai développé un sens de l\'écoute et un esprit créatif grâce à la musique (saxophone), que je pratique depuis plus de 14 ans.'
+
+const itemsSchool = ref<TimelineItem[]>([
     {
-        date: 'Mar 15, 2025',
-        title: 'Project Kickoff',
-        description: 'Kicked off the project with team alignment. Set up project milestones and allocated resources.',
-        icon: 'i-lucide-rocket'
+        date: 'Septembre 2023 - Ajourd\'hui',
+        title: 'BUT Métiers du Multimédia et de l\'Internet',
+        description: 'IUT de Haguenau - Parcours : Développement web & Dispositifs Interactifs',
+        icon: 'i-lucide-laptop'
     },
     {
-        date: 'Mar 22 2025',
-        title: 'Design Phase',
-        description: 'User research and design workshops. Created wireframes and prototypes for user testing.',
-        icon: 'i-lucide-palette'
+        date: 'Septembre 2020 - Juin 2023',
+        title: 'Baccalauréat général - Mention Bien',
+        description: 'Lycée Fustel de Coulanges, Strasbourg - Spécialités : Mathématiques & Physique-Chimie',
+        icon: 'i-lucide-school'
     },
     {
-        date: 'Mar 29 2025',
-        title: 'Development Sprint',
-        description: 'Frontend and backend development. Implemented core features and integrated with APIs.',
-        icon: 'i-lucide-code'
-    },
-    {
-        date: 'Apr 5 2025',
-        title: 'Testing & Deployment',
-        description: 'QA testing and performance optimization. Deployed the application to production.',
-        icon: 'i-lucide-check-circle'
+        date: '2013 - 2024',
+        title: 'Diplôme du COA (cycle orientation amateur)',
+        description: 'Cité de la Musique et de la Danse, Strasbourg - Cursus : Saxophone',
+        icon: 'i-lucide-piano'
     }
+])
+
+const itemsJob = ref<TimelineItem[]>([
+    {
+        date: 'Septembre 2025 - Aujourd\'hui',
+        title: 'Alternante Développeuse web Front-end & Conceptrice UX',
+        description: 'Logitud Solutions, Strasbourg - Conception et développement de logiciels métiers pour la fonction publique.',
+        icon: 'i-lucide-square-code'
+    },
 ])
 </script>
 
 <template>
-    <section class="w-full h-full flex flex-col justify-center items-center">
-        <UPageHero title="A propos de moi" description="Description sur moi" orientation="horizontal">
+    <section class="w-full h-full">
+        <UPageHero title="A propos de moi" :description="aboutMe" orientation="horizontal" :ui="{
+            container: 'lg:py-32 lg:flex lg:flex-row',
+        }">
             <img src="https://static.vecteezy.com/system/resources/thumbnails/057/068/323/small/single-fresh-red-strawberry-on-table-green-background-food-fruit-sweet-macro-juicy-plant-image-photo.jpg"
                 alt="fraise" class="rounded-full w-80 aspect-square object-cover">
         </UPageHero>
-        <UTimeline :items="items" />
+        <div class="flex flex-col gap-5">
+            <h2 class="text-3xl font-bold">Mes formations</h2>
+            <UTimeline :items="itemsSchool" />
+        </div>
+        <div class="flex flex-col gap-5">
+            <h2 class="text-3xl font-bold">Mes formations</h2>
+            <UTimeline :items="itemsJob" />
+        </div>
     </section>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/icon'
+  ],
   css: ['~/assets/css/main.css'],
 })
